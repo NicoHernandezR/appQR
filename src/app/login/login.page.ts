5394fbeae531cd7705bc2221f26923c3274e84cd
Sol0D <francoshicoalien@gmail.com>
2023-08-30 01:17:17 -0400

    Implementando animaciones y login con la base de datos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { NavController, ToastController } from '@ionic/angular';
+import { ApiService } from '../api.service';
+
 
 
 @Component({
@@ -15,10 +17,12 @@ export class LoginPage implements OnInit {
     password: ""
   }
   usuarios: string[];
-
+  info_usuario : any;
+  email: string = ''
   
   field: string = "";
-  constructor(public navCtrl: NavController,private router: Router, public toastController: ToastController) {
+  constructor(public navCtrl: NavController,private router: Router, 
+    public toastController: ToastController, private apiService : ApiService ) {
     this.usuarios = [this.user.usuario];
 
    }
@@ -26,9 +30,22 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  aceptar() {
-    this.router.navigate(['/home']);
-      };
+  async aceptar() {
+    console.log(this.user.usuario)
+    console.log(this.user.password)
+    this.info_usuario = await this.apiService.comprobarAlumno(this.user.usuario, this.user.password)
+
+    console.log(this.info_usuario.items.length)
+
+    if (this.info_usuario.items.length === 0){
+      return;
+    }
+
+    this.info_usuario = this.info_usuario.items[0]
+    console.log(this.info_usuario)
+    this.router.navigate(['/home'])
+
+  }
   validateModel(model: any) {
     for (var [key, value] of Object.entries(model)) {
       if (value == "") {
@@ -45,5 +62,9 @@ export class LoginPage implements OnInit {
     });
     toast.present();
   }
-
+  isValidEmail(): boolean {
+    const emailPattern = /[A-Za-z0-9]+@[A-Za-z0-9]+\.[a-z]{2,4}/;
+    return emailPattern.test(this.email);
+  }
 }
+  
